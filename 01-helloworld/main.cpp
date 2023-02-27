ea9ce07d5d8109e8da22faab13914e82f80ea40c
Max Elkin <maksim.v.elkin@rt.ru>
2023-02-27 20:20:02 +0300

    check push path in actions

--- 01-helloworld/main.cpp
+++ 01-helloworld/main.cpp
@@ -4,6 +4,6 @@
 
 int main(int, char**) {
     std::cout << "Version: " << version_lib::version() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!!!" << std::endl;
     return 0;
 }
